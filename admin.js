60dae4be4c8d31201dc4005fb428a686609f0615
Vaibhav <codingvaibhav247@gmail.com>
2024-05-08 02:57:35 +0530

    Added MVC structure for eventsection

--- admin.js
+++ admin.js
@@ -17,6 +17,8 @@ const connectDB = require('./config/dbconfig.js');
 const adminRouter = require("./routes/adminroutes.js");
 const herosectionRouter = require("./routes/herosectionroutes.js");
 const countdownRouter = require('./routes/countdownroute.js');
+const eventsectionRouter = require("./routes/eventsectionroute.js");
+
 connectDB;
 
 
@@ -81,126 +83,7 @@ app.use("/admin/countdownsection",countdownRouter);
 
 //event section starts here
 
-
-//get request for events route
-app.get("/events", async (req, res) => {
-  let events = await Event.find();
-  console.log(events);
-  res.render("events.ejs", { events });
-})
-
-
-//post request on eventsroute
-app.post("/events",upload.single('myFile'), async (req, res) => {
-  let { date, subtitle, title } = req.body;
-  date = date.toString();
-  subtitle = subtitle.toString();
-  title = title.toString();
-  let myFile = req.file.originalname;
-  let fileLocation = path.join("./uploads", myFile);
-  // console.log(req.body);
-  // console.log(date, subtitle, title, image, myFile);
-  fs.readFile(fileLocation, async (err, data) => {
-
-    if (err) throw err; // Fail if the file can't be read.
-    imagekit.upload({
-      file: data, //required
-      fileName: myFile, //required
-    }, async function (error, result) {
-      if (error) console.log(error);
-      else {
-        // console.log(result);
-        let image = result.url;
-        let imageid = result.fileId;
-        let data = new Event({ date: date, image: image, subtitle: subtitle, title: title, imageid: imageid });
-        await data.save();
-        console.log(data);
-        fs.unlinkSync(fileLocation);
-        res.redirect("/events");
-      }
-    });
-  });
-})
-
-//get request to edit events section
-app.get("/editevent/:id", async (req, res) => {
-  let { id } = req.params;
-  id = id.toString();
-  let data = await Event.find({ _id: id });
-  console.log(data);
-  res.render("editevent.ejs", { data });
-})
-
-//patch request on editevents redirect to events route
-app.patch("/editevent/:id", upload.single('myFile'), async (req, res) => {
-  let { id } = req.params;
-  let { date, subtitle, title,imagecheckbox} = req.body;
-  date = date.toString();
-  subtitle = subtitle.toString();
-  title = title.toString();
-  
-
-  console.log(req.body);
-
-  if (!imagecheckbox) {
-    let document = await Event.findOneAndUpdate({ _id: id }, { date: date, subtitle: subtitle, title: title });
-    res.redirect("/events");
-  }
-  else {
-    let myFile = req.file.originalname;
-    let fileLocation = path.join("./uploads", myFile);
-    fs.readFile(fileLocation, async (err, data) => {
-      if (err) throw err;   // Fail if the file can't be read.
-      imagekit.upload({
-        file: data,   //required
-        fileName: myFile,   //required
-      },
-        async function (error, result) {
-          if (error) console.log(error);
-          else {
-            // console.log(result);
-            let image = result.url;
-            let imageid = result.fileId;
-            let document = await Event.findOneAndReplace({ _id: id }, { date: date, subtitle: subtitle, title: title, image: image, imageid: imageid });
-
-            let oldimageid = document.imageid;
-
-            imagekit.deleteFile(oldimageid)
-              .then(response => {
-                console.log(response);
-              })
-              .catch(error => {
-                console.log(error);
-              });
-              fs.unlinkSync(fileLocation);
-              res.redirect("/events");
-          }
-        });
-    });
-  }
-
-}
-);
-
-//delete request on eventroute and again redirect to same page
-app.delete("/events/:id", async (req, res) => {
-  let { id } = req.params;
-  id = id.toString();
-  let delData = await Event.findByIdAndDelete(id);
-
-  let imageid = delData.imageid;
-
-  imagekit.deleteFile(imageid)
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  // console.log(id);
-  res.redirect("/events");
-})
-
+app.use("/admin/eventsection",eventsectionRouter);
 
 //events route ends here
 
---
+++ routes/eventsectionroute.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { storage } = require("../config/imagekitconfig.js");
+const { showEvents, createEvent, destroyEvent, renderEditForm, updateEvent } = require("../controllers/eventsectioncontroller.js")
+const upload = multer({ storage: storage });
+
+
+router.route("/")
+    //get request for events route
+    .get(showEvents)
+
+    //post request on eventsroute
+    .post(upload.single('myFile'), createEvent)
+
+
+
+router.route("/edit/:id")
+    //get request to edit events section
+    .get(renderEditForm)
+
+    //patch request on editevents redirect to events route
+    .patch(upload.single('myFile'), updateEvent)
+
+
+//delete request on eventroute and again redirect to same page
+router.delete("/:id", destroyEvent)
+
+module.exports = router;
