d4539798f06072b4ac0381d171a4e6c07c17ec31
Vaibhav <codingvaibhav247@gmail.com>
2024-05-11 06:09:26 +0530

    added utils folder containing custom error handling and async route error handling

---
+++ utils/wrapAsyncAndExpressError.js
@@ -0,0 +1,19 @@
+class ExpressError extends Error {
+    constructor(statusCode, message) {
+        super();
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
+
+/*wrapAsync is a function taking asyncronous function as input and then return bhi function kar raha hai by executing the
+callback function as input if error catch and call the next error handing middleware jo ki admin.js mai hai*/
+const wrapAsync = (fnx) => {
+    return (req,res,next) =>{
+        fnx(req,res,next).catch((err) => next(err));
+    }
+}
+
+module.exports = wrapAsync
+
+module.exports = {ExpressError,wrapAsync};
