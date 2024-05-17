1f3fced489d5bf49e420838ca5b598e797e16b05
Vaibhav <codingvaibhav247@gmail.com>
2024-05-18 00:08:22 +0530

    added workshop routes

--- models/workshop.js
+++ models/workshop.js
@@ -1,16 +1,44 @@
 const mongoose = require("mongoose");
-
-const loginSchema = new mongoose.Schema({
-    name: {
-      type : String,
-      required:true
+const Registration = require("./registration");
+const User = require("./user.js");
+const wrapAsync = require("../utils/wrapAsyncAndExpressError.js");
+const workshopSchema = new mongoose.Schema({
+    label: {
+        type: String,
+        required: true
     },
-    email: {
-      type : String,
-      required:true
-    }
-  });
+    title: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    registrations: [{
+        type: mongoose.Schema.ObjectId,
+        ref: "Registration"
+    }]
+});
 
-const Workshop = mongoose.model("Workshop",loginSchema);
+
+workshopSchema.post("findOneAndDelete", async (workshopData) => {
+    if (workshopData) {
+        console.log(workshopData);
+        const workshopRegistrations = await workshopData.populate("registrations");
+        console.log("haa bhai mai hi hu", workshopRegistrations);
+        await Registration.deleteMany({ _id: { $in: workshopData.registrations } });
+        console.log(workshopRegistrations.registrations);
+        let UserIds = workshopRegistrations.registrations.map((registration) => registration.user); //array bana liya id ka
+        console.log("user id", UserIds);
+        let delUsers = await User.updateMany({ _id: { $in: UserIds } }, { $pull: { workshops: workshopData._id } });
+    }
+})
+
+const Workshop = mongoose.model("Workshop", workshopSchema);
 
 module.exports = Workshop;
--- routes/homepageroute.js
+++ routes/homepageroute.js
@@ -1,28 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const Heroslider = require("../models/herosection.js");
-const Countdown = require("../models/countdown.js");
 const Workshop = require("../models/workshop.js");
+const Registration = require("../models/registration.js");
 const Specialslider = require('../models/specialsection.js');
 const Testimonial = require('../models/testimonials.js');
 const Event = require("../models/events.js");
 const Booking = require("../models/booking.js");
+const User = require("../models/user.js");
+
 const { validateRegistration, validateBookings } = require("../middlewares/homepagemiddleware.js");
 const { wrapAsync } = require("../utils/wrapAsyncAndExpressError.js")
-const {isLogedIn} = require("../middlewares/authmiddlewares.js");
+const { isLogedIn } = require("../middlewares/authmiddlewares.js");
 
 router.get('/', wrapAsync(async (req, res) => {
     let heroSliders = await Heroslider.find();
-    let countdown = await Countdown.find();
+    let workshop = await Workshop.find();
+    let currTime = new Date();
+    let isWorkshop = false;
+    if(workshop[workshop.length - 1].time > currTime) isWorkshop = true;
     let specialSection = await Specialslider.find();
     let testimonials = await Testimonial.find();
     let events = await Event.find();
-    let allSection = { heroSliders, countdown, specialSection, testimonials, events,user:req.user };
-    console.log(allSection);
+    let allSection = { heroSliders, workshop, specialSection, testimonials, events, user: req.user ,isWorkshop:isWorkshop};
+    console.log(req.user);
     res.render("homepage/index.ejs", { allSection });
 }));
 
-router.post('/',isLogedIn ,validateBookings, wrapAsync(async (req, res) => {
+router.post('/', isLogedIn, validateBookings, wrapAsync(async (req, res) => {
     const name = req.body.name;
     const phone = req.body.phone;
     const person = req.body.person;
@@ -31,25 +36,36 @@ router.post('/',isLogedIn ,validateBookings, wrapAsync(async (req, res) => {
     const message = req.body.message;
     const newbooking = new Booking({ name, phone, person, date, time, message });
     await newbooking.save();
-    // const popupScript = `
-    //   <script>
-    //     alert("Yeah! ${name} Your Table is Booked!");
-    //   </script>
-    // `;
     res.redirect("/");
 }));
 
 
 
-router.post('/signup',isLogedIn, validateRegistration, wrapAsync(async (req, res) => {
-    const { userName, userEmail } = req.body;
-    const user = await Workshop.find({ email: userEmail });
-    if (user.length == 0) {
-        const newRegistration = new Workshop({ name: userName, email: userEmail });
+router.post('/register/:id', isLogedIn, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let flag = true;
+    console.log("register");
+    req.user.workshops.forEach((workshop) => {
+        if (workshop._id == id) {
+            flag = false;
+            return res.send({ name: req.user.fullname, status: "Already Registered!" });
+        }
+    })
+    if (flag) {
+        const { userMessage, userPhone } = req.body;
+
+        const user = await User.findById(req.user._id);
+        const workshop = await Workshop.findById(id);
+        user.workshops.push(workshop);
+        await User.findByIdAndUpdate(req.user._id, user);
+
+        const newRegistration = new Registration({ phoneNumber: userPhone, message: userMessage });
+        newRegistration.user = user._id;
+        newRegistration.workshop = workshop._id;
         await newRegistration.save();
-        res.send({ name: userName, status: "Your Registration is Successful!" });
-    } else {
-        res.send({ name: userName, status: "Already Registered!" });
+        workshop.registrations.push(newRegistration);
+        await Workshop.findByIdAndUpdate(id, workshop,{new:true});
+        res.send({ name: req.user.fullname, status: "Your Registration is Successful!" });
     }
 }));
 
