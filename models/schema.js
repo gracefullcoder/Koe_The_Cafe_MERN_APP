a6e726aba94c5a4726b1d12431de437dfbf6ef25
Vaibhav <codingvaibhav247@gmail.com>
2024-05-23 00:35:36 +0530

    Implemented server side validation for all the schemas

--- models/schema.js
+++ models/schema.js
@@ -1,14 +1,30 @@
 const Joi = require('joi');
 
 const registrationSchema = Joi.object({
-    userName: Joi.string().required(),
-    userEmail: Joi.string().email().required()
+    userMessage: Joi.string(),
+    userPhone: Joi.string().required().regex(/^[0-9]{10}$/).messages({ 'string.pattern.base': `Phone number must have 10 digits.` }).required()
 });
+
+const signUpSchema = Joi.object({
+    fullname: Joi.string().required(),
+    gender: Joi.string().valid('Male', 'Female', 'transgender', 'others').required(),
+    DOB :Joi.date().required(),
+    profilepicture:Joi.object().allow({},null),
+    useremail: Joi.string().email().required(),
+    userpassword:Joi.string().required()
+})
+
+const updateUserSchema = Joi.object({
+    fullname:Joi.string().required(),
+    DOB:Joi.date().required(),
+    gender:Joi.string().valid('Male', 'Female', 'transgender', 'others').required(),
+    imagecheckbox:Joi.string()
+})
 
 const bookingsSchema = Joi.object({
     name: Joi.string().required(),
     phone: Joi.string().regex(/^[0-9]{10}$/).messages({ 'string.pattern.base': `Phone number must have 10 digits.` }).required(),
-    person: Joi.string().required().messages({ 'reason': "Thala for reason!" }),
+    person: Joi.number().required().min(1).max(10000).messages({ 'reason': "Thala for reason!" }),
     time: Joi.string().required(),
     reservationdate: Joi.date().required(),
     message: Joi.string().required()
@@ -25,12 +41,20 @@ const eventSchema = Joi.object({
     date: Joi.date().required(),
     subtitle : Joi.string().required(),
     title : Joi.string().required(),
-    myFile : Joi.string().required
+    myFile : Joi.string().required()
+})
+
+const editEventSchema = Joi.object({
+    date: Joi.date().required(),
+    subtitle : Joi.string().required(),
+    title : Joi.string().required(),
+    myFile : Joi.string().allow("", null),
+    imagecheckbox : Joi.string()
 })
 
 const testimonialSchema = Joi.object({
-    name : Joi.string().required(),
     review :Joi.string().required(),
+    suggestion : Joi.string(),
     myFile : Joi.string().allow("",null)
 })
 
@@ -50,4 +74,4 @@ const editSchema = Joi.object({
     myFile: Joi.string().allow("", null)
 })
 
-module.exports = { registrationSchema, bookingsSchema, newSectionSchema,eventSchema,testimonialSchema,editSchema,workshopSchema};
+module.exports = {signUpSchema,updateUserSchema, registrationSchema, bookingsSchema, newSectionSchema,eventSchema,editEventSchema,testimonialSchema,editSchema,workshopSchema};
