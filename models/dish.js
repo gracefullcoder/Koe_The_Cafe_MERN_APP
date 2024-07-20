a74677a4bb5781c6277b2d1971d7c8fe2c0951ec
Vaibhav <codingvaibhav247@gmail.com>
2024-07-20 23:59:48 +0530

    Created Dish Model for menu section

---
+++ models/dish.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const dishSchema = new Schema(
+    {
+        dishName: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        dishImage: {
+            type: String
+        },
+        imageId: {
+            type: String
+        },
+        price: {
+            type: Number
+        },
+        tag: {
+            type: String
+        },
+        reviews: [{
+            _id: false,
+            comment: {
+                type: String,
+            },
+            rating: Number
+        }]
+    }
+);
+
+const Dish = mongoose.model("Dish", dishSchema);
+
+module.exports = Dish;
