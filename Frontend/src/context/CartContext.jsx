9ede89bbe9a5a2e1b30156694c1d81b192462c57
Vaibhav <codingvaibhav247@gmail.com>
2024-07-22 16:46:54 +0530

    Implemented Cart Context and Provider

---
+++ Frontend/src/context/CartContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+
+    const [cart, setCart] = useState({ active: false, orders: [] });
+    const { user } = useAuthContext();
+    const [loading, setLoading] = useState(user ? false : true);
+
+    useEffect(() => {
+
+        const renderCart = async () => {
+            const url = `${import.meta.env.VITE_SERVER_ENDPOINT}/cart`;
+            const fetchUrl = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                credentials: "include"
+            })
+
+            const responseData = await fetchUrl.json();
+            console.log(responseData);
+            setCart({ active: false, orders: responseData.cart });
+            setLoading(true);
+        }
+
+        user && renderCart();
+    }, [])
+
+    return (<CartContext.Provider value={[cart, setCart]}>
+        {loading ? children : <h1>Getting cart ready!</h1>}
+    </CartContext.Provider >)
+}
+
+export const useCartContext = () => useContext(CartContext);
