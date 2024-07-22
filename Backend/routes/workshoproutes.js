c605bc1476721298685b5788282348d8da1d6861
Vaibhav <codingvaibhav247@gmail.com>
2024-07-22 14:37:10 +0530

    Updated Workshop routes

--- Backend/routes/workshoproutes.js
+++ Backend/routes/workshoproutes.js
@@ -1,58 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const Workshop = require('../models/workshop.js');
-const {validateWorkShopEdit} = require("../middlewares/adminmiddlewares.js");
-const {wrapAsync} = require("../utils/wrapAsyncAndExpressError.js");
+const { validateWorkShopEdit } = require("../middlewares/adminmiddlewares.js");
+const { wrapAsync } = require("../utils/wrapAsyncAndExpressError.js");
 
-router.get("/", wrapAsync(async(req, res) => {
+router.get("/", wrapAsync(async (req, res) => {
     let workshops = await Workshop.find();
     let currTime = new Date();
     let currWorkshops = [];
     let pastWorkshops = [];
 
-    // time = new Date(time); //agar schema mai string hota tho aise parse karke use karte
-
     workshops.forEach((workshop) => {
-        if(currTime < workshop.time){
+        if (currTime < workshop.time) {
             currWorkshops.push(workshop);
-        }else{
+        } else {
             pastWorkshops.push(workshop);
         }
     })
-    // console.log(workshops);
-    res.render("workshop/workshop.ejs", { currWorkshops,pastWorkshops});
+    res.status(200).json({ currWorkshops, pastWorkshops });
 }))
 
-router.get("/edit/:id",wrapAsync(async (req,res) => {
-    let {id} = req.params;
-    let workshopData = await Workshop.findById(id);
-    // console.log(workshopData);
-    res.render("workshop/editworkshop.ejs",{workshopData});
-}))
-
-router.delete("/:id" ,wrapAsync(async (req,res) => {
-    let {id} = req.params;
+router.delete("/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let workshop = await Workshop.findByIdAndDelete(id);
-    // console.log(workshop);
-    res.redirect("/admin/workshopsection");
+    res.status(200).json({success:true,message:"Workshop Deleted Successfully!"})
 }))
 
-router.post("/",validateWorkShopEdit,wrapAsync(async(req,res) => {
+router.post("/", validateWorkShopEdit, wrapAsync(async (req, res) => {
     let { label, title, text, date, time } = req.body;
-    // console.log(req.body);
     label = label.toString();
     title = title.toString();
-    time = date.toString() + "T" + time.toString(); //T lagana must hai
+    time = date.toString() + "T" + time.toString();
     text = text.toString();
-    let workshopDetails = {label: label, title: title, time: time, text: text};
+    let workshopDetails = { label: label, title: title, time: time, text: text };
     let newWorkshop = new Workshop(workshopDetails);
-    // console.log(newWorkshop);
     await newWorkshop.save();
-    res.redirect("/admin/workshopsection");
+    res.status(200).json({success:true,message:"Workshop Added Successfully"})
 }))
 
 
-router.patch("/edit/:id",validateWorkShopEdit, wrapAsync(async (req, res) => {
+router.patch("/edit/:id", validateWorkShopEdit, wrapAsync(async (req, res) => {
+    console.log("edit workshop")
     let { id } = req.params;
     id = id.toString();
     let { label, title, text, date, time } = req.body;
@@ -61,7 +49,7 @@ router.patch("/edit/:id",validateWorkShopEdit, wrapAsync(async (req, res) => {
     time = date.toString() + "T" + time.toString();
     text = text.toString();
     await Workshop.findOneAndUpdate({ _id: id }, { label: label, title: title, time: time, text: text, new: true });
-    res.redirect("/admin/workshopsection");
+    res.status(200).json({success:true,message:"Workshop Updated Successfully"})
 }));
 
 module.exports = router;
