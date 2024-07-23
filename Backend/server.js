9414518c2533225ce439f543cb52900fa7f504de
Vaibhav <codingvaibhav247@gmail.com>
2024-07-23 21:26:01 +0530

    Updated Socket setup on server

--- Backend/server.js
+++ Backend/server.js
@@ -50,14 +50,9 @@ const orderRouter = require("./routes/orderroutes.js");
 const trafficRouter = require("./routes/trafficroutes.js");
 
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "/views"));
 app.set("trust proxy", 1);
 
-app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
 const cors = require('cors')
 
@@ -169,125 +164,13 @@ server.listen(PORT, () => {
 });
 
 //sockets code
-const Booking = require('./models/booking.js');
-const orderIdToSocket = new Map();
 
 const io = new Server(server, {
   cors: corsOptions
 })
-
+const socketConnections = require("./socket.js");
+socketConnections(io);
 app.set('socket.io', io);
-
-const seatTracking = new Map();
-
-async function setOverlappingBookings(slotDetails) {
-  const parseTime = (time) => {
-    return { hrs: parseInt(time.slice(0, 2)), minutes: parseInt(time.slice(3, 5)) };
-  }
-
-  const userStartTime = parseTime(slotDetails.startTime);
-  const userEndTime = parseTime(slotDetails.endTime);
-
-  let bookings = await Booking.find({ date: slotDetails.date });
-
-  const timeInterval = (userTime, bookedTime) => {
-    if (userTime.hrs != bookedTime.hrs) return (userTime.hrs > bookedTime.hrs ? true : false);
-    return (userTime.minutes >= bookedTime.minutes ? true : false);
-  }
-
-  console.log(bookings, slotDetails);
-
-  const bookedSeats = bookings.map((booking) => {
-
-    const bookedStartTime = parseTime(booking.startTime);
-    const bookedEndTime = parseTime(booking.endTime);
-
-
-    if (!(timeInterval(userStartTime, bookedStartTime) && timeInterval(userStartTime, bookedEndTime))) {
-      return booking;
-    }
-
-    if (!(timeInterval(bookedStartTime, userStartTime) && timeInterval(bookedStartTime, userEndTime))) {
-      return booking;
-    }
-
-  })
-  console.log(bookedSeats);
-
-  const newSeatMapping = new Map();
-  bookedSeats.forEach((booking) => {
-    booking.seats.forEach((seat) => {
-      newSeatMapping.set(seat, "booked");
-    })
-  })
-  console.log("i m the new", newSeatMapping);
-  return newSeatMapping;
-}
-
-io.on("connection", (socket) => {
-  let timeOver = null;
-
-  socket.on("get-reservation", async (slotDetails) => {
-    const seatMapping = await setOverlappingBookings(slotDetails);
-    socket.emit("seats-layout", [...seatMapping]);
-    console.log("recieved client send matrix");
-    timeOver = setTimeout(() => {
-      socket.emit("disconnect-user")
-    }, 100000);
-  })
-
-  socket.on("remove-selected-seats", ({ selectedSeats, userId, slotDetails }) => {
-    console.log("here", selectedSeats);
-    socket.broadcast.emit("seats-updated", { seatKey: selectedSeats, userId, slotDetails, status: "disconnected" })
-  })
-
-  socket.on("seat-selected", ({ seatKey, userId, slotDetails }) => {
-    console.log("seatselected", seatKey);
-
-    io.emit("seats-updated", { seatKey, status: "added", user: userId, slotDetails });
-  })
-
-  socket.on("seat-removed", ({ seatKey, userId, slotDetails }) => {
-    io.emit("seats-updated", { seatKey, status: "removed", user: userId, slotDetails });
-  })
-
-  socket.on("seat-booked", async ({ selectedSeats, userId, slotDetails }) => {
-    const bookingData = {};
-
-    selectedSeats.forEach((seat) => {
-      seatTracking.set(seat, "booked");
-      const table = "table".concat(seat[0].charCodeAt(0) - 64);
-      bookingData[`${table}.${seat}`] = userId;
-    })
-    console.log(bookingData);
-    clearTimeout(timeOver);
-    io.emit("seat-confirmed", { selectedSeats, user: userId, slotDetails });
-  })
-
-  socket.on("my-orders", (orderIds) => {
-    const socketId = socket.id;
-    orderIds.forEach((id) => orderIdToSocket.set(id, socketId));
-    console.log(orderIdToSocket);
-  })
-
-  socket.on("order-updated", (data) => {
-    console.log("order updated");
-    const socketId = orderIdToSocket.get(data.orderId);
-    io.to(socketId).emit("updated-status", data);
-  })
-
-  socket.on("remove-orders", (orderIds, callback) => {
-    orderIds.forEach(orderId => {
-      orderIdToSocket.delete(orderId);
-    })
-    console.log(orderIds, " remove then => upadted socket to orderid map map ", orderIdToSocket);
-    callback();
-  })
-
-  socket.on("disconnect", () => {
-    console.log("User with socket ", socket.id, " got disconected")
-  })
-})
 
 //apis
 app.use("/", homeRouter);
---
+++ Backend/socket.js
@@ -0,0 +1,118 @@
+const Booking = require('./models/booking.js');
+
+async function setOverlappingBookings(slotDetails) {
+    const parseTime = (time) => {
+        return { hrs: parseInt(time.slice(0, 2)), minutes: parseInt(time.slice(3, 5)) };
+    }
+
+    const userStartTime = parseTime(slotDetails.startTime);
+    const userEndTime = parseTime(slotDetails.endTime);
+
+    let bookings = await Booking.find({ date: slotDetails.date });
+
+    const timeInterval = (userTime, bookedTime) => {
+        if (userTime.hrs != bookedTime.hrs) return (userTime.hrs > bookedTime.hrs ? true : false);
+        return (userTime.minutes >= bookedTime.minutes ? true : false);
+    }
+
+    console.log(bookings, slotDetails);
+
+    const bookedSeats = bookings.map((booking) => {
+
+        const bookedStartTime = parseTime(booking.startTime);
+        const bookedEndTime = parseTime(booking.endTime);
+
+
+        if (!(timeInterval(userStartTime, bookedStartTime) && timeInterval(userStartTime, bookedEndTime))) {
+            return booking;
+        }
+
+        if (!(timeInterval(bookedStartTime, userStartTime) && timeInterval(bookedStartTime, userEndTime))) {
+            return booking;
+        }
+
+    })
+    console.log(bookedSeats);
+
+    const newSeatMapping = new Map();
+    bookedSeats.forEach((booking) => {
+        booking.seats.forEach((seat) => {
+            newSeatMapping.set(seat, "booked");
+        })
+    })
+    console.log("i m the new", newSeatMapping);
+    return newSeatMapping;
+}
+
+const socketConnections = (io) => {
+    const orderIdToSocket = new Map();
+    const seatTracking = new Map();
+
+    io.on("connection", (socket) => {
+        let timeOver = null;
+
+        socket.on("get-reservation", async (slotDetails) => {
+            const seatMapping = await setOverlappingBookings(slotDetails);
+            socket.emit("seats-layout", [...seatMapping]);
+            console.log("recieved client send matrix");
+            timeOver = setTimeout(() => {
+                socket.emit("disconnect-user")
+            }, 100000);
+        })
+
+        socket.on("remove-selected-seats", ({ selectedSeats, userId, slotDetails }) => {
+            console.log("here", selectedSeats);
+            socket.broadcast.emit("seats-updated", { seatKey: selectedSeats, userId, slotDetails, status: "disconnected" })
+        })
+
+        socket.on("seat-selected", ({ seatKey, userId, slotDetails }) => {
+            console.log("seatselected", seatKey);
+
+            io.emit("seats-updated", { seatKey, status: "added", user: userId, slotDetails });
+        })
+
+        socket.on("seat-removed", ({ seatKey, userId, slotDetails }) => {
+            io.emit("seats-updated", { seatKey, status: "removed", user: userId, slotDetails });
+        })
+
+        socket.on("seat-booked", async ({ selectedSeats, userId, slotDetails }) => {
+            const bookingData = {};
+
+            selectedSeats.forEach((seat) => {
+                seatTracking.set(seat, "booked");
+                const table = "table".concat(seat[0].charCodeAt(0) - 64);
+                bookingData[`${table}.${seat}`] = userId;
+            })
+            console.log(bookingData);
+            clearTimeout(timeOver);
+            io.emit("seat-confirmed", { selectedSeats, user: userId, slotDetails });
+        })
+
+        socket.on("my-orders", (orderIds) => {
+            const socketId = socket.id;
+            orderIds.forEach((id) => orderIdToSocket.set(id, socketId));
+            console.log(orderIdToSocket);
+        })
+
+        socket.on("order-updated", (data) => {
+            console.log("order updated");
+            const socketId = orderIdToSocket.get(data.orderId);
+            io.to(socketId).emit("updated-status", data);
+        })
+
+        socket.on("remove-orders", (orderIds, callback) => {
+            orderIds.forEach(orderId => {
+                orderIdToSocket.delete(orderId);
+            })
+            console.log(orderIds, " remove then => upadted socket to orderid map map ", orderIdToSocket);
+            callback();
+        })
+
+        socket.on("disconnect", () => {
+            console.log("User with socket ", socket.id, " got disconected")
+        })
+    })
+}
+
+
+module.exports = socketConnections;
