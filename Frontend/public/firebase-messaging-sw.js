5eafcd00f0aa58ad35fa9ebd8f233714e4c9a212
Vaibhav <codingvaibhav247@gmail.com>
2024-08-27 13:41:10 +0530

    Removed bugs

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
 
 
 const firebaseConfig = {
